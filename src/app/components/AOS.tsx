b54e673cd981dcf2ca453c5922dc2dc44ca9d5c2
clientesweb <alexisifran32@gmail.com>
2025-02-25 03:13:43 -0300

    Actualizar AOS.tsx

--- src/app/components/AOS.tsx
+++ src/app/components/AOS.tsx
@@ -1,20 +1,21 @@
 "use client"
 
-import { useEffect } from 'react'
+import { useEffect } from "react"
 import AOS from "aos"
-import "aos/dist/aos.css";
+import "aos/dist/aos.css"
 
 export const AOSInit = () => {
   useEffect(() => {
     AOS.init({
-        easing: 'ease-out-quad',
-        duration: 1000,
-        once: true,
-        mirror: false,
-        offset: 10,
-        delay: 400,
-    });
+      easing: "ease-out-quad",
+      duration: 1000,
+      once: true,
+      mirror: false,
+      offset: 10,
+      delay: 400,
+    })
   }, [])
 
   return null
 }
+
