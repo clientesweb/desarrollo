22ba3c49f1ef47b5a58d40a803111046822e2fbf
clientesweb <alexisifran32@gmail.com>
2025-02-25 03:12:42 -0300

    Actualizar tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,67 @@
-import type { Config } from 'tailwindcss'
-
+import type { Config } from "tailwindcss"
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend:{
+    extend: {
       colors: {
-        hotpink: '#fff1da',
-        green: '#5dae1d',
-        orange: '#e79310',
-        blue: '#181e4b',
-        litepurple: '#5e6282',
-        litegrey: '#686d77',
-        grey: '#DFD7F9',
-        litedark: '#212832',
-        offwhite: '#F5F5F5',
-       }
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+          dark: "#00A9AC",
+          light: "#40E0E5",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+          dark: "#E64A2E",
+          light: "#FFA07A",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: "#94A3B8",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: "#FFFFFF",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-poppins)", "sans-serif"],
+        display: ["var(--font-volkhov)", "serif"],
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 export default config
+
