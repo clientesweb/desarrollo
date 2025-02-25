59b512ce0783d7d2995a4dce774368f178bc73bd
clientesweb <alexisifran32@gmail.com>
2025-02-25 04:29:30 -0300

    Actualizar Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -36,13 +36,13 @@ const Header = () => {
 
   return (
     <nav
-      className={`py-5 px-[5%] top-0 w-full fixed flex items-center justify-between z-20 ${headerBackground} transition-all duration-300 special-font`}
+      className={`py-3 px-[5%] top-0 w-full fixed flex items-center justify-between z-20 ${headerBackground} transition-all duration-300 special-font`}
     >
-      <Link href="/" className="w-[80px]">
+      <Link href="/" className="w-[60px]">
         <Image
           src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/logo-duality-domain-r6m55SQWTLflCkvPu7WmEi9dgTru6x.png"
-          width={80}
-          height={80}
+          width={60}
+          height={60}
           alt="Duality Domain Logo"
           className="w-full"
           loading="eager"
@@ -50,7 +50,7 @@ const Header = () => {
       </Link>
 
       <section
-        className={`${isMenuOpen ? "left-0" : "left-[-100%]"} nav-transition md:transition-none absolute top-[68px] md:top-0 md:left-0 md:relative md:flex flex-col md:flex-row items-start md:items-center justify-start md:justify-between gap-4 lg:gap-8 text-[16px] text-accent font-bold bg-background/95 md:bg-transparent pl-[5%] md:pl-auto py-10 md:py-0 w-full md:w-auto backdrop-blur-sm md:backdrop-blur-none`}
+        className={`${isMenuOpen ? "left-0" : "left-[-100%]"} nav-transition md:transition-none absolute top-[54px] md:top-0 md:left-0 md:relative md:flex flex-col md:flex-row items-start md:items-center justify-start md:justify-between gap-4 lg:gap-8 text-[16px] text-accent font-bold bg-background/95 md:bg-transparent pl-[5%] md:pl-auto py-10 md:py-0 w-full md:w-auto backdrop-blur-sm md:backdrop-blur-none`}
       >
         <div className="flex flex-col md:flex-row items-start md:items-center justify-start md:justify-between gap-4 lg:gap-8">
           <a href="#services" className="hover:text-primary transition-colors duration-200">
