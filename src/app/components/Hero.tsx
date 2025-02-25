22aef9a71c2244ea50a16598d5a51af57b83dcbf
clientesweb <alexisifran32@gmail.com>
2025-02-25 03:49:50 -0300

    Actualizar Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,15 +7,15 @@ const Hero = () => {
     <main className="relative w-full flex flex-col md:flex-row items-center justify-between py-[80px] md:py-[100px] px-[5%] bg bg-right-top bg-no-repeat bg-contain">
       <section className="w-full md:w-[50%] flex flex-col items-center md:items-start justify-center md:justify-start gap-3">
         <p className="text-[14px] text-primary font-bold special-font" data-aos="fade-down">
-          BEST SOLUTIONS FOR YOUR BUSINESS
+          LAS MEJORES SOLUCIONES PARA TU NEGOCIO
         </p>
         <h1
           className="text-4xl md:text-5xl xl:text-6xl text-accent font-extrabold w-full lg:w-[80%] text-center md:text-left"
           data-aos="fade-down"
         >
-          Transform,
+          Transforma,
           <span className="relative w-full px-1">
-            <span className="relative z-10 w-full gradient-text">innovate</span>
+            <span className="relative z-10 w-full gradient-text">innova</span>
             <Image
               src="/img/line-decore.png"
               width={100}
@@ -25,14 +25,14 @@ const Hero = () => {
               loading="eager"
             />
           </span>
-          and elevate your digital presence
+          y eleva tu presencia digital
         </h1>
         <p
           className="text-[14px] text-muted font-bold w-full lg:w-[80%] xl:w-[65%] text-center md:text-left special-font"
           data-aos="fade-up"
         >
-          We craft cutting-edge solutions that blend creativity with technical excellence. Our team of experts delivers
-          custom software that drives your business forward.
+          Creamos soluciones de vanguardia que combinan creatividad con excelencia técnica. Nuestro equipo de expertos
+          entrega software personalizado que impulsa tu negocio.
         </p>
 
         <div className="flex items-center gap-4 special-font" data-aos="fade-up">
@@ -40,13 +40,13 @@ const Hero = () => {
             href="#category"
             className="py-3 px-5 rounded-md bg-secondary text-white text-[14px] font-bold flex items-center hover:bg-secondary-dark transition-colors duration-300"
           >
-            Explore Services
+            Explorar Servicios
           </a>
           <Link
             href="/"
             className="py-3 px-5 rounded-md text-accent text-[14px] font-bold flex items-center gap-2 hover:shadow-md hover:bg-background-light transition-colors duration-300"
           >
-            <IoIosPlay className="text-2xl text-white p-[7px] rounded-full bg-primary" /> Watch Demo
+            <IoIosPlay className="text-2xl text-white p-[7px] rounded-full bg-primary" /> Ver Demo
           </Link>
         </div>
       </section>
